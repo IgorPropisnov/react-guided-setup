5d21e1a056078850d52ada3ca690f0894c711c38
Igor Propisnov <i.propisnov@dobosolution.de>
2019-07-14 15:42:47 +0200

    :sparkles: Added markup and logic
    Buttons for switching between the steps with disabled for max and min stat, Some conditional step rendering, stylings

--- src/components/GuidedSetup/guidedSetup.js
+++ src/components/GuidedSetup/guidedSetup.js
@@ -1,7 +1,52 @@
 import React from 'react'
+import styles from './guidedSetup.module.scss';
 
-const guidedSetup = () => (
-    <p>Guided Setup</p>
-)
+class GuidedSetup extends React.Component {
 
-export default guidedSetup
+    state = {
+        step: 1,
+        maxStep: 5,
+        minStep: 1
+    }
+
+    nextStep = () => {
+        if (this.state.step < this.state.maxStep) {
+            this.setState(prevState => {
+                return { step: prevState.step + 1 }
+            })
+        }
+    }
+
+    prevStep = () => {
+        if (this.state.step > this.state.minStep) {
+            this.setState(prevState => {
+                return { step: prevState.step - 1 }
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className={styles.wrapper} >
+                <div className={styles.stepWrapper}>
+                    {
+                        this.state.step && (
+                            <>
+                                {this.state.step === 1 && (<div className={styles.step}>Step 1</div>)}
+                                {this.state.step === 2 && (<div className={styles.step}>Step 2</div>)}
+                                {this.state.step === 3 && (<div className={styles.step}>Step 3</div>)}
+                                {this.state.step === 4 && (<div className={styles.step}>Step 4</div>)}
+                                {this.state.step === 5 && (<div className={styles.step}>Step 5</div>)}
+                            </>
+                        )
+                    }
+
+                </div>
+                <button disabled={this.state.step === this.state.minStep} onClick={this.prevStep} className={styles.button}>Prev Step</button>
+                <button disabled={this.state.step === this.state.maxStep} onClick={this.nextStep} className={styles.button}>Next Step</button>
+            </div>
+        )
+    }
+}
+
+export default GuidedSetup
